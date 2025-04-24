6f16912175f62f6defd4f7466eff3122a8917a3a
ivabarlay <ivabarlay@alum.us.es>
2025-04-24 19:43:46 +0200

    hola mundo avanzado

--- practica/mpi/src/hola_mundo_avanzado.c
+++ practica/mpi/src/hola_mundo_avanzado.c
@@ -1,8 +1,17 @@
+#include <mpi.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-  
-  printf("Soy el proceso %i de %i: !Hola mundo!\n");
- 
-  return(0);
+int main(int argc, char *argv[]) {
+  int num_procesos;
+  int id_proceso;
+
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &id_proceso);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procesos);
+  printf("Soy el proceso %d de %d: !Hola mundo!\n", id_proceso, num_procesos);
+
+  MPI_Finalize();
+
+  return (0);
 }
