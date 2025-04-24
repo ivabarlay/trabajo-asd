e3460175b7b5fc0a36d119c92596bd2d4f966001
ivabarlay <ivabarlay@alum.us.es>
2025-04-24 16:48:30 +0200

    mcpi paralelo y script para probar

--- practica/openmp/src/mcpi_par.c
+++ practica/openmp/src/mcpi_par.c
@@ -4,25 +4,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+#include <string.h>
 
 int main (int argc, char *argv[]) {
-  unsigned short xi[3] = { 1, 2, 3 };
+  unsigned short xi[3];
   unsigned long long count = 0;
   unsigned long long i;
   unsigned long long samples;
   double x, y;
+  struct drand48_data buffer;
   samples=3000000; /*Valor por defecto*/
 
   if (argc >1)
-    samples = atoll(argv[1]);
+    samples = atoi(argv[1]);
     
-
-  for (i = 0; i < samples; ++i) {
-    x = erand48(xi);
-    y = erand48(xi);
-    if (x*x+y*y <= 1.0) {
-      ++count;
-    }
+# pragma omp parallel private(i, xi, buffer)
+  {
+      xi[0] = xi[1] = xi[2] = omp_get_thread_num();
+      memset(&buffer, 0, sizeof(buffer));
+# pragma omp parallel for reduction(+:count)
+      for (int i = 0; i < samples; ++i) {
+          erand48_r(xi, &buffer, &x);
+          erand48_r(xi, &buffer, &y);
+          if (x*x+y*y <= 1.0) {
+              ++count;
+          }
+      }
   }
 
   printf ("Valor estimado de pi:  %.7f\n", 4.0*count/samples);
