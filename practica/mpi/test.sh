235a51d49cd43169886372c2012aedd845783d71
ivabarlay <ivabarlay@proton.me>
2025-04-25 11:18:18 +0200

    test hecho

--- practica/mpi/test.sh
+++ practica/mpi/test.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "TODO"
+echo "Hola mundo avanzado: "
+echo "\n///////////////////"
+mpirun -np 4 bin/hola_mundo_avanzado
+echo "///////////////////\n"
 
+echo "saludos: "
+echo "\n///////////////////"
+mpirun -np 4 bin/saludos
+echo "///////////////////\n"
 
+echo "Saludos en anillo: "
+echo "\n///////////////////"
+mpirun -np 4 bin/saludos_anillo
+echo "///////////////////\n"
