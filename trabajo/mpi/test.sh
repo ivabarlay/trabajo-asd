5cec28e9cd677155240f01f5a6efcdc596469b1c
ivabarlay <ivabarlay@alum.us.es>
2025-05-17 00:03:26 +0200

    test ahora puedes pasar argumento o por defecto

--- trabajo/mpi/test.sh
+++ trabajo/mpi/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 runner=(mpirun -np 4)
+if [[ -z "$1" ]]
+then
+    samples=10000000
+else
+    samples=$1
+fi
 
 # Crear csv
 echo "method,implementation,threads,time (s),pi_value" > mpi_pi.csv
@@ -9,8 +15,8 @@ echo "method,implementation,threads,time (s),pi_value" > mpi_mcpi.csv
 make
 
 # PI MPI
-${runner[@]} ./bin/pi_par >> mpi_pi.csv
-${runner[@]} ./bin/mcpi_par >> mpi_mcpi.csv
+${runner[@]} ./bin/pi_par $1 >> mpi_pi.csv
+${runner[@]} ./bin/mcpi_par $1 >> mpi_mcpi.csv
 
 
 
