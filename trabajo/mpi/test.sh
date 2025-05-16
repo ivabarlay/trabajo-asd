6115e0515dfca1969a19cc580966e23c936c8943
ivabarlay <ivabarlay@alum.us.es>
2025-05-17 00:56:49 +0200

    quitado array de iteraciones pero ahora solo un sample y un thread

--- trabajo/mpi/test.sh
+++ trabajo/mpi/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-RUNNER=(mpirun -np 4)
 SRC_FOLDER="./results"
 
 # Check if argument is passed
@@ -7,7 +6,15 @@ SRC_FOLDER="./results"
 if [[ -z "$1" ]]; then
     samples=10000000
 else
-    samples=$@
+    samples=$1
+fi
+
+# Check if number of threads is passed
+
+if [[ -z "$2" ]]; then
+    threads=4
+else
+    threads=$2
 fi
 
 # Check if results directory exits, then create it if it is not
@@ -27,9 +34,14 @@ make bear
 
 for SAMPLE in ${samples}
 do
-    echo "Número de iteraciones: " $SAMPLE
-    ${RUNNER[@]} ./bin/pi_par $SAMPLE >> ${SRC_FOLDER}/mpi_result.csv
-    ${RUNNER[@]} ./bin/mcpi_par $SAMPLE >> ${SRC_FOLDER}/mpi_result.csv
+    for THREAD in ${threads}
+    do
+        runner=(mpirun -np ${THREAD})
+        echo "Número de iteraciones: " ${SAMPLE}
+        echo "Número de hilos: " ${THREAD}
+        ${runner[@]} ./bin/pi_par ${SAMPLE} >> ${SRC_FOLDER}/mpi_result.csv
+        ${runner[@]} ./bin/mcpi_par ${SAMPLE} >> ${SRC_FOLDER}/mpi_result.csv
+    done
 done
 
 
