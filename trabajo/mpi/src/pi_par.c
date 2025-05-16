24ab24c9a9d54a0ff0fab6ff12d5df6288fd0741
ivabarlay <ivabarlay@alum.us.es>
2025-05-16 18:39:22 +0200

    número por defecto a pi integral

--- trabajo/mpi/src/pi_par.c
+++ trabajo/mpi/src/pi_par.c
@@ -3,39 +3,46 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int rank, size;
-  int i;
-  double x, pi, sum = 0.0;
-  long numSteps = atol(argv[1]);
-  double step = 1.0 / (double)numSteps;
-  long partition;
-  double start, end;
+    int rank, size;
+    int i;
+    double x, pi, sum = 0.0;
+    /* long numSteps = atol(argv[1]); */
+    long numSteps;
+    if (argc > 1)
+        numSteps = atoll(argv[1]);
+    else {
+        numSteps = 1000000;
+    }
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (rank == 0) {
-      start = MPI_Wtime();
-  }
+    double step = 1.0 / (double)numSteps;
+    long partition;
+    double start, end;
 
-  partition = (long)(numSteps / size);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        start = MPI_Wtime();
+    }
 
-  /* printf("rank: %d\n", rank); */
-  /* printf("size: %d\n", size); */
-  /* printf("partition: %ld\n", partition); */
+    partition = (long)(numSteps / size);
 
-  for (i = rank * partition; i < rank * partition + partition; ++i) {
-    x = (i + 0.5) * step;
-    sum += 4.0 / (1.0 + x * x);
-  }
+    /* printf("rank: %d\n", rank); */
+    /* printf("size: %d\n", size); */
+    /* printf("partition: %ld\n", partition); */
 
-  MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-      end = MPI_Wtime();
-    pi = pi * step;
-    /* printf("Valor de pi: %f\n", pi); */
-    printf("Integration,MPI,%d,%f,%f", size, end-start, pi);
-  }
-  MPI_Finalize();
-  return 0;
+    for (i = rank * partition; i < rank * partition + partition; ++i) {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        end = MPI_Wtime();
+        pi = pi * step;
+        /* printf("Valor de pi: %f\n", pi); */
+        printf("Integration,MPI,%d,%f,%f", size, end - start, pi);
+    }
+    MPI_Finalize();
+    return 0;
 }
