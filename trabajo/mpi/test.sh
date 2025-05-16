8ef1d7e80511f3ecdaabb4943a287f422a5b54f2
ivabarlay <ivabarlay@alum.us.es>
2025-05-17 00:16:50 +0200

    Añadido script para limpiar

--- trabajo/mpi/test.sh
+++ trabajo/mpi/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-runner=(mpirun -np 4)
+RUNNER=(mpirun -np 4)
+SRC_FOLDER="./results"
+
+# Check if argument is passed
+
 if [[ -z "$1" ]]
 then
     samples=10000000
@@ -7,16 +11,22 @@ else
     samples=$1
 fi
 
-# Crear csv
-echo "method,implementation,threads,time (s),pi_value" > mpi_pi.csv
-echo "method,implementation,threads,time (s),pi_value" > mpi_mcpi.csv
+# Check if results directory exits, then create it if it is not
 
-# Compilar todo
-make
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir ./results
+fi
+
+# Create csv
+echo "method,implementation,threads,time (s),pi_value" > ${SRC_FOLDER}/mpi_pi.csv
+echo "method,implementation,threads,time (s),pi_value" > ${SRC_FOLDER}/mpi_mcpi.csv
+
+# Compile everything
+make bear
 
 # PI MPI
-${runner[@]} ./bin/pi_par $1 >> mpi_pi.csv
-${runner[@]} ./bin/mcpi_par $1 >> mpi_mcpi.csv
+${RUNNER[@]} ./bin/pi_par $1 >> ${SRC_FOLDER}/mpi_pi.csv
+${RUNNER[@]} ./bin/mcpi_par $1 >> ${SRC_FOLDER}/mpi_mcpi.csv
 
 
 
