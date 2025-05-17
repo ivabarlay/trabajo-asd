bc40ae88ac26abe7697930542e37118e4b308fff
ivabarlay <ivabarlay@alum.us.es>
2025-05-17 13:50:59 +0200

    tests para omp también

--- trabajo/openmp/src/pi_par.c
+++ trabajo/openmp/src/pi_par.c
@@ -3,20 +3,47 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  double x, pi, sum = 0.0;
-  long numSteps = atol(argv[1]);
-  double step = 1.0 / (double)numSteps;
-  int i;
-#pragma omp parallel private(x, i) reduction(+ : sum)
-  {
-#pragma omp for
-    for (i = 0; i < numSteps; ++i) {
-      x = (i + 0.5) * step;
-      sum += 4.0 / (1.0 + x * x);
-    }
-  }
+    double x, pi, sum = 0.0;
+    long long numSteps;
+    double step;
+    int i;
+    double start, end;
+    int size;
+    int num_threads;
 
-  pi = step * sum;
-  printf("Valor de pi: %f\n", pi);
-  return 0;
+    if (argc > 2)
+        num_threads = atoll(argv[2]);
+    else {
+        num_threads = 4;
+    }
+
+    if (argc > 1)
+        numSteps = atoll(argv[1]);
+    else {
+        numSteps = 1000000;
+    }
+
+    step = 1.0 / (double)numSteps;
+
+    omp_set_num_threads(num_threads);
+
+    start = omp_get_wtime();
+
+
+#pragma omp parallel private(x, i) reduction(+ : sum)
+    size = omp_get_num_threads();
+    printf("%d", size);
+    {
+#pragma omp for
+        for (i = 0; i < numSteps; ++i) {
+            x = (i + 0.5) * step;
+            sum += 4.0 / (1.0 + x * x);
+        }
+    }
+
+    pi = step * sum;
+
+    end = omp_get_wtime();
+    printf("Integration,OMP,%d,%f,%f,%lld\n", size, end - start, pi, numSteps);
+    return 0;
 }
---
+++ trabajo/openmp/test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+SRC_FOLDER="./results"
+
+# Check if argument is passed
+
+if [[ -z "$1" ]]; then
+    samples=10000000
+else
+    samples=$1
+    samples_list=()
+    samples_tens=10
+
+    # Generates a list of numbers starting at 10, ending
+    # at the nearest 10th power less than the argument
+    while [[ $samples_tens -le $samples ]]; do
+        samples_list+=("$samples_tens")
+        (( samples_tens= samples_tens * 10 ))
+    done
+    samples=${samples_list[@]}
+fi
+
+# Check if number of threads is passed
+
+if [[ -z "$2" ]]; then
+    threads=4
+else
+    # Generate a list beggining from 1 and ending in the number of threads
+    threads=$2
+    threads_list=()
+    for ((i = 1; i <= ${threads}; i++)); do
+        threads_list+=("$i")
+    done
+    threads=${threads_list[@]}
+    
+fi
+
+# Check if results directory exits, then create it if it is not
+
+if [ ! -d "$SRC_FOLDER" ]; then
+    mkdir ${SRC_FOLDER}
+fi
+
+# Create csv
+echo "method,implementation,threads,time (s),pi_value,samples" > ${SRC_FOLDER}/omp_result.csv
+# echo "method,implementation,threads,time (s),pi_value" > ${SRC_FOLDER}/mpi_mcpi.csv
+
+# Compile everything
+make bear
+
+# PI MPI
+
+for SAMPLE in ${samples}
+do
+    for THREAD in ${threads}
+    do
+        echo "Número de iteraciones: " ${SAMPLE}
+        echo "Número de hilos: " ${THREAD}
+        ./bin/pi_par ${SAMPLE} ${THREAD} >> ${SRC_FOLDER}/omp_result.csv
+        ./bin/mcpi_par ${SAMPLE} ${THREAD} >> ${SRC_FOLDER}/omp_result.csv
+    done
+done
+
+
